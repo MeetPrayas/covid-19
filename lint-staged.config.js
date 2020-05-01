a83e91886f6f570581baf1393dd16090b2199a37
MeetPrayas <ag.prayas@gmail.com>
2020-05-01 20:05:01 +0530

    Updates

--- lint-staged.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  "*.+(js|jsx)": ['prettier --write "**/*.+(js|jsx)"'],
-  "*.+(json|yml|yaml|md)": ['prettier --write "**/*.+(json|yml|yaml|md)"'],
-}
---
+++ precache-manifest.978a5ba0a2f49d765f1b4e8588b0d0dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "709f3555043d9ffa376055561d1d956d",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "8a600d277291faabae52",
+    "url": "/covid-19/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "7648b55c290c41dd981c",
+    "url": "/covid-19/static/js/2.4169dc26.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/covid-19/static/js/2.4169dc26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a600d277291faabae52",
+    "url": "/covid-19/static/js/main.86c19395.chunk.js"
+  },
+  {
+    "revision": "e84dd164dd3b06d1310b",
+    "url": "/covid-19/static/js/runtime-main.6ca52c11.js"
+  }
+]);
