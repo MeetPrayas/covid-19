6372254d0ffdda66296c29c30310b0126a242d8f
Sudhanshu <perfectsudh@gmail.com>
2020-05-01 17:09:51 +0530

    chore: a little bit of housekeeping
    added prettier, husky and lint-staged. prettified the code.
    also added serve as a dev depedency.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 /* App.js */
-import React, { useState, useEffect } from "react";
-import Graph from "./view/graph";
-import SearchAppBar from "./view/AppBar";
-import Cards from "./view/Cards";
-import axios from "axios";
-import { makeStyles, useTheme } from "@material-ui/core/styles";
-import CustomizedTables from "./view/Table";
-import Grid from "@material-ui/core/Grid";
+import React, { useState, useEffect } from "react"
+import Graph from "./view/graph"
+import SearchAppBar from "./view/AppBar"
+import Cards from "./view/Cards"
+import axios from "axios"
+import { makeStyles, useTheme } from "@material-ui/core/styles"
+import CustomizedTables from "./view/Table"
+import Grid from "@material-ui/core/Grid"
 
 // const useStyles = makeStyles((theme) => ({
 //   root: {
@@ -21,22 +21,22 @@ import Grid from "@material-ui/core/Grid";
 // }))
 
 export default function App(props) {
-  const [data, setData] = useState(null);
-  const [stateData, setStateData] = useState(null);
+  const [data, setData] = useState(null)
+  const [stateData, setStateData] = useState(null)
   useEffect(() => {
     axios.get("https://api.covid19india.org/data.json").then((response) => {
-      setData(response);
-      console.log(response);
-    });
+      setData(response)
+      console.log(response)
+    })
     axios
       .get("https://api.covid19india.org/state_district_wise.json")
       .then((response) => {
-        setStateData(response);
-        console.log(response);
-      });
-  }, []);
+        setStateData(response)
+        console.log(response)
+      })
+  }, [])
 
-  console.log(data, stateData);
+  console.log(data, stateData)
   return (
     <div>
       {data && stateData ? (
@@ -47,7 +47,7 @@ export default function App(props) {
       <div style={{ margin: "0 5%" }}>
         <h2 align="center">Current status in India</h2>
         {data && stateData ? <Cards data={data} stateData={stateData} /> : ""}
-        <Grid container style={{marginTop: "10px"}} spacing={2}>
+        <Grid container style={{ marginTop: "10px" }} spacing={2}>
           <Grid item xs={12} xs={12} sm={12} md={12} lg={6} xl={6}>
             {data && stateData ? (
               <Graph data={data} stateData={stateData} />
@@ -65,5 +65,5 @@ export default function App(props) {
         </Grid>
       </div>
     </div>
-  );
+  )
 }
